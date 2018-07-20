3e6f1f750d1e055bd5fdc612650ff263ddab7034
jackiehanyang <jkhanjob@gmail.com>
2018-07-20 11:02:20 -0700

    switch to VS

---
+++ Employee.Management/Scripts/searchData.js
@@ -0,0 +1,9 @@
+function searchCompanyFromDB() {
+    var x = document.getElementById("inputCompanyName").value;
+    var y = document.getElementById("inputEmployeeName").value;
+    var z = document.getElementById("inputEmployeeAge").value;
+    document.getElementById("demo").innerHTML = x;
+    document.getElementById("demo1").innerHTML = y;
+    document.getElementById("demo2").innerHTML = z;
+
+}
